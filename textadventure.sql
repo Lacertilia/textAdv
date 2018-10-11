dfa79dd03f8d2cff340e4524ca2d3ba25ac7acf3
Lacertilia <nivmizzet1>
2018-10-11 10:39:06 +0200

    test

---
+++ textadventure.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 11 okt 2018 kl 10:24
+-- Serverversion: 10.1.29-MariaDB
+-- PHP-version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `textadventure`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `story`
+--
+
+CREATE TABLE `story` (
+  `id` int(16) UNSIGNED NOT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `place` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `story`
+--
+
+INSERT INTO `story` (`id`, `text`, `place`) VALUES
+(1, 'Vem gillar du?', 'Här'),
+(2, 'Synd, personen gillar inte dig och du kommer dö ensam', ''),
+(3, 'Är du säker på det?', ''),
+(4, 'Du spenderar ditt liv lycklig och ensam. Du dör tids nog lyckligt där du vill vara, med dig själv.', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `storylinks`
+--
+
+CREATE TABLE `storylinks` (
+  `id` int(16) UNSIGNED NOT NULL,
+  `storyid` int(10) UNSIGNED NOT NULL,
+  `target` int(10) UNSIGNED NOT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `storylinks`
+--
+
+INSERT INTO `storylinks` (`id`, `storyid`, `target`, `text`) VALUES
+(1, 1, 2, 'Jörgen'),
+(2, 1, 2, 'Violetta'),
+(3, 1, 2, 'Doktorn'),
+(4, 1, 3, 'Dig Själv'),
+(5, 3, 4, 'Ja!'),
+(6, 3, 1, 'Nej');
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `story`
+--
+ALTER TABLE `story`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index för tabell `storylinks`
+--
+ALTER TABLE `storylinks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `storyid` (`storyid`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `story`
+--
+ALTER TABLE `story`
+  MODIFY `id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT för tabell `storylinks`
+--
+ALTER TABLE `storylinks`
+  MODIFY `id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
