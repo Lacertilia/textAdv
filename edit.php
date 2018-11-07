1cd2d8c4d328c5aabdbbaa919295bf002dfa47f4
Lacertilia <te16.rasmus.mattas@umea.nti.se>
2018-11-07 09:25:47 +0100

    LastUpdate

--- edit.php
+++ edit.php
@@ -19,7 +19,77 @@
 		<main class="content">
 			<section>
 				<h1>Redigera</h1>
+
 				<?php
+				
+				include_once 'include/dbinfo.php';
+
+				$_SESSION['loggedIn'] = false;
+
+				
+				if($_SESSION['loggedIn'] == true){
+
+					echo '<form action="" method="POST">
+						<textarea name="storyAdd" id="storyAdd" cols="30" rows="10"></textarea>
+						<textarea name="placeAdd" id="placeAdd" cols="30" rows="1"></textarea>
+						<input type="submit" name="submit" id="submit" Value="Lägg Till">
+					</form>
+					';
+					
+					$dbh = new PDO("mysql:host=" . $host . ";dbname=" . $db . ";charset=utf8mb4", $dbuser, $dbpass);
+
+					if(isset($_POST['submit'])){
+						$filteredText = filter_input(INPUT_POST, $_POST['storyAdd'], FILTER_SANITIZE_STRING);
+						$filteredPlace = filter_input(INPUT_POST, $_POST['placeAdd'], FILTER_SANITIZE_STRING);
+						$stmt = $dbh->prepare("INSERT INTO story (text, place) VALUES :text, :place");
+						$stmt->bindParam(':text', $filteredText);
+						$stmt->bindParam(':place', $filteredPlace);
+						$stmt->execute();
+						header("location: edit.php");
+					}
+				}else{	
+					
+					if(isset($_POST['username']) && isset($_POST['password'])){
+
+						$filteredUsername = filter_input(INPUT_POST, $_POST['username'], FILTER_SANITIZE_STRING);
+						$password = $_POST['password'];
+
+						$dbh = new PDO("mysql:host=" . $host . ";dbname=" . $db2 . ";charset=utf8mb4", $dbuser, $dbpass);
+
+						$stmt = $dbh->prepare("SELECT * FROM login WHERE username = :username");
+						$stmt->bindParam(':username', $filteredUsername);
+						$stmt->execute();
+
+						$row = $stmt->fetch(PDO::FETCH_ASSOC);
+						var_dump($stmt);
+						var_dump($password);
+						var_dump($row);
+						var_dump($row['password']);
+						var_dump(password_verify($password, $row['password']));
+
+						if(password_verify($password, $row['password'])){
+							$_SESSION['loggedIn'] = true;
+							header('location: edit.php');
+						}else{
+							echo '<h1>Nu blev det fel</h1>';
+						}
+
+					}else{
+
+						echo '
+						<h1>Logga In</h1>
+						<form action="" method="POST">
+							<p>Användarnamn<input type="text" name="username" id="username"></p>
+							<p>Lösenord<input type="password" name="password" id="password"></p>
+							<input type="submit" name="submit" id="submit" value="Logga In!">
+						</form>
+					';
+					}
+
+
+
+
+				}
 				// TODO protect with your login
 				// add, edit, delete pages & events
 				// skriv ut en lista över sidor
---
+++ passwordHash.php
@@ -0,0 +1,6 @@
+<?php
+
+
+echo password_hash('secure', PASSWORD_BCRYPT);
+
+?>
