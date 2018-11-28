261a8d4ed6187f822fb0185c353578ed2f1f5128
Lacertilia <te16.rasmus.mattas@umea.nti.se>
2018-11-28 08:41:21 +0100

    liteBootstrap

--- edit.php
+++ edit.php
@@ -8,14 +8,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">  
 		<title>Soloäventyr - Redigera</title>
 		<link href="https://fonts.googleapis.com/css?family=Merriweather%7cMerriweather+Sans" rel="stylesheet"> 
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
-	<body>
-		<nav id="navbar">
-			<a href="index.php">Hem</a>
-			<a href="play.php">Spela</a>
-			<a class="active" href="edit.php">Redigera</a>
-		</nav>	
+	<body style="background-color:grey; text-align:center;">
+		<nav class="navbar navbar-extend-lg bg-dark sticky-top">
+			<a href="index.php" class="btn btn-light">Hem</a>
+			<a href="play.php" class="btn btn-danger">Spela</a>
+			<a href="edit.php" class="btn btn-light">Redigera</a>
+		</nav>
 		<main class="content">
 			<section>
 				<h1>Redigera</h1>
@@ -24,7 +24,7 @@
 				
 				include_once 'include/dbinfo.php';
 
-				$_SESSION['loggedIn'] = false;
+				$_SESSION['loggedIn'] = true;
 
 				
 				if($_SESSION['loggedIn'] == true){
@@ -41,11 +41,11 @@
 					if(isset($_POST['submit'])){
 						$filteredText = filter_input(INPUT_POST, $_POST['storyAdd'], FILTER_SANITIZE_STRING);
 						$filteredPlace = filter_input(INPUT_POST, $_POST['placeAdd'], FILTER_SANITIZE_STRING);
-						$stmt = $dbh->prepare("INSERT INTO story (text, place) VALUES :text, :place");
+						$stmt = $dbh->prepare("INSERT INTO story (text, place) VALUES :text, :place)");
 						$stmt->bindParam(':text', $filteredText);
 						$stmt->bindParam(':place', $filteredPlace);
 						$stmt->execute();
-						header("location: edit.php");
+						header("Location: edit.php");
 					}
 				}else{	
 					
@@ -69,7 +69,7 @@
 
 						if(password_verify($password, $row['password'])){
 							$_SESSION['loggedIn'] = true;
-							header('location: edit.php');
+							header('Location: edit.php');
 						}else{
 							echo '<h1>Nu blev det fel</h1>';
 						}
@@ -100,5 +100,8 @@
 			</section>
 		</main>
 		<script src="js/navbar.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	</body>
 </html>
--- play.php
+++ play.php
@@ -7,14 +7,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">  
 	<title>Soloäventyr - Spela</title>
-	<link href="https://fonts.googleapis.com/css?family=Merriweather%7cMerriweather+Sans" rel="stylesheet"> 
-	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather%7cMerriweather+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body>
-<nav id="navbar">
-	<a href="index.php">Hem</a>
-	<a class="active" href="play.php">Spela</a>
-	<a href="edit.php">Redigera</a>
+<body style="background-color:grey; text-align:center;">
+<nav class="navbar navbar-extend-lg bg-dark sticky-top">
+	<a href="index.php" class="btn btn-light">Hem</a>
+	<a href="play.php" class="btn btn-danger">Spela</a>
+	<a href="edit.php" class="btn btn-light">Redigera</a>
 </nav>	
 <main class="content">
 	<section>
@@ -37,7 +37,7 @@
 
 		$_SESSION['page'] = $filteredPage;
 
-		if($_SESSION['page'] == 4 || $_SESSION['page'] == 2 || $_SESSION['page'] == 5 || $_SESSION['page'] == 8){
+		if($_SESSION['page'] == 4 || $_SESSION['page'] == 2 || $_SESSION['page'] == 5 || $_SESSION['page'] == 8 || $_SESSION['page'] == 12){
 			$_SESSION['page'] = 1;
 		}
 
@@ -47,7 +47,7 @@
 
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-		echo "<p>" . $row['text'] . "</p>";
+		echo "<p class=\"font-weight-bold\" style=\"font-size:1.2em;\">" . $row['text'] . "</p>";
 
 		$stmt = $dbh->prepare("SELECT * FROM storylinks WHERE storyid = :id");
 		$stmt->bindParam(':id', $filteredPage);
@@ -56,7 +56,7 @@
 		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 		foreach($row as $val){
-			echo "<a href=\"?page=" . $val['target'] .  "\">" . $val['text'] . "</a><br>";
+			echo "<a href=\"?page=" . $val['target'] .  "\" class=\"btn btn-dark mr-2\">" . $val['text'] . "</a>";
 		}
 
 	} else if(isset($_SESSION['page'])) {
@@ -69,7 +69,7 @@
 
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-		echo "<p>" . $row['text'] . "</p>";
+		echo "<p class=\"font-weight-bold\" style=\"font-size:1.2em;\">" . $row['text'] . "</p>";
 
 		$stmt = $dbh->prepare("SELECT * FROM storylinks WHERE storyid = :id");
 		$stmt->bindParam(':id', $filteredPage);
@@ -78,12 +78,12 @@
 		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 		foreach($row as $val){
-			echo "<a href=\"?page=" . $val['target'] . "\">" . $val['text'] . "</a><br>";
+			echo "<a href=\"?page=" . $val['target'] . "\" class=\"btn btn-dark mr-2\">" . $val['text'] . "</a>";
 		}
 
 	} else {
 		
-		$stmt = $dbh->prepare("SELECT * FROM story WHERE id = 1");
+		/*$stmt = $dbh->prepare("SELECT * FROM story WHERE id = 1");
 		$stmt->execute();
 
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -98,12 +98,17 @@
 		foreach($row as $val){
 			echo "<a href=\"?page=" . $val['target'] .  "\">" . $val['text'] . "</a><br>";
 		}
-
+*/
+		$_SESSION['page'] = 1;
+		header("Location: play.php");	
 	}
 
 ?>
 </section>
 </main>
 <script src="js/navbar.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
